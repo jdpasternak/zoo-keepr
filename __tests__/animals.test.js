5eb8853d6ec3b1ea6849a337c306ffe1087a0488
Jake Pasterank <jdp.pasternak@gmail.com>
2022-01-14 17:33:12 -1000

    Separate functions, begin writing tests

---
+++ __tests__/animals.test.js
@@ -0,0 +1,10 @@
+const {
+  filterByQuery,
+  findById,
+  createNewAnimal,
+  validateAnimal,
+} = require("../lib/animals");
+
+descirbe("filterByQuery", () => {
+    describe('should return all animals with carnivore diet')
+})
